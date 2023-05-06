c840e92f8676f461d2eccd66dad3939759409713
karyanayandi <halo@yandi.me>
2023-05-06 17:27:32 +0700

    feat: add use-callback-ref hooks

---
+++ packages/hooks/use-callback-ref/src/index.ts
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+export function useCallbackRef<T extends (...args: any[]) => any>(
+  callback: T | undefined,
+  deps: React.DependencyList = [],
+) {
+  const callbackRef = React.useRef(callback)
+
+  React.useEffect(() => {
+    callbackRef.current = callback
+  })
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  return React.useCallback(((...args) => callbackRef.current?.(...args)) as T, deps)
+}
